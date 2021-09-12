f9b4d891d3eed1a2106fb5102a4814268b82d680
Alex <correovidal@outlook.com>
2021-09-12 19:55:39 +0200

    Adding help argument

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     args = parseator()
 
     if args.h:
-        print()
+        print(args.h)
 
--- src/args.py
+++ src/args.py
@@ -1,10 +1,25 @@
 import argparse
 
+'''
+IMPORTANTE REVISAR DOCS DE ARGPARSE: https://docs.python.org/3/library/argparse.html
+'''
+nameProg = "SuiteMinigames"
+usageProg = "%(prog)s [options]"
+epilogProg = "See you soon"
+defaultProg = "-hang"
+descProg = "Testing description"
+
+
 def parseator():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-h", "--hang", type=str,help="Ejecutar juego de arhorcado")
-    parser.add_argument("-m","--mine", type=str,help="Ejecutar juego de buscaminas")
-    parser.add_argument("-s","--sched", type=str,help="Ejecuta la aplicación de agenda")
-    args = parser.parse_args()
+    parser = argparse.ArgumentParser(prog = nameProg, 
+    					usage = usageProg, 
+    					epilog = epilogProg,
+    					description=descProg
+    					)
+    parser.add_argument("-hang", type=str,help="Ejecutar juego de arhorcado")
+    parser.add_argument("-mine", type=str,help="Ejecutar juego de buscaminas")
+    parser.add_argument("-sched", type=str,help="Ejecuta la aplicación de agenda")
+    parser.add_argument("-help", "--h", type=str,help="Mostrar la ayuda")
+    args = parser.parse_args()  
 
     return args
